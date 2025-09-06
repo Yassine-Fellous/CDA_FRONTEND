160a7a42baa6dc6d511eec066a7fe1ad423f6a6e
yassine fellous <kenshi@MacBook-Air-de-yassine.local>
2025-09-07 01:59:09 +0200

    add center point and popup in map

--- src/components/Map/MapView.jsx
+++ src/components/Map/MapView.jsx
@@ -292,7 +292,7 @@ export default function MapView() {
         'icon-size': [
           'case',
           ['==', ['get', 'id'], selectedId || ''],
-          0.3, // ✅ MÊME TAILLE QUE LE PIN ORANGE (était 0.4)
+          0.2, // ✅ MÊME TAILLE QUE LE PIN ORANGE (était 0.4)
           0.3  // Taille normale
         ]
       }
